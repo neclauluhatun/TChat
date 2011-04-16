82e3f5daedc8b66a36bc8ac4b1a3d3f2f2a0ae15
unknown <Mehmet@.(none)>
2011-04-16 04:48:32 +0300

    SecondPart

--- auth.py
+++ auth.py
@@ -4,82 +4,98 @@ import sys, os
 import datetime
 import subprocess
 import pickle
+import thread
 
 class loadTweets(object):
-   
+    def sendMessages(userName,self):
+            message = str(raw_input("message?\n"))
+            self.api.send_direct_message(screen_name = userName, text = message)
+    def getMessage(self):
+            mesajlar = self.api.direct_messages(count=10)
+            for msg in mesajlar:
+               print "Mesaj geldi = %s !" %msg.text
     def fetchTweets(self, since_id=None):
-        if since_id:
-            tweets = self.api.home_timeline(since_id, count=10)
-        else:
-            #tweets = self.api.home_timeline(screen_name="ozansener",count=10)
-            tweets = self.api.mentions(count=10)
+##        if since_id:
+##            tweets = self.api.home_timeline(since_id, count=10)
+##        else:
+##            #tweets = self.api.home_timeline(screen_name="ozansener",count=10)
+##            tweets = self.api.mentions(count=10)
+        followers = self.api.followers(screen_name='msencer')
         # parse each incoming tweet
         ts = []
+        users={}
         authors = []
-        for tweet in tweets:
-           if tweet.author.screen_name=="ozansener" :
-            t = {
-            'author': tweet.author.screen_name,
-            'contributors': tweet.contributors,
-            'coordinates': tweet.coordinates,
-            'created_at': tweet.created_at,
-            # 'destroy': tweet.destroy,
-            # 'favorite': tweet.favorite,
-            'favorited': tweet.favorited,
-            'geo': tweet.geo,
-            'id': tweet.id,
-            'in_reply_to_screen_name': tweet.in_reply_to_screen_name,
-            'in_reply_to_status_id': tweet.in_reply_to_status_id,
-            'in_reply_to_user_id': tweet.in_reply_to_user_id,
-            # 'parse': tweet.parse,
-            # 'parse_list': tweet.parse_list,
-            'place': tweet.place,
-            # 'retweet': dir(tweet.retweet),
-            # 'retweets': dir(tweet.retweets),
-            'source': tweet.source,
-            # 'source_url': tweet.source_url,
-            'text': tweet.text,
-            'truncated': tweet.truncated,
-            'user': tweet.user.screen_name,
-            }
-            u = {
-            '_id': tweet.author.screen_name, # use as mongo primary key
-            'contributors_enabled': tweet.author.contributors_enabled, 
-            'created_at': tweet.author.created_at, 
-            'description': tweet.author.description, 
-            'favourites_count': tweet.author.favourites_count, # beware the british
-            'follow_request_sent': tweet.author.follow_request_sent, 
-            'followers_count': tweet.author.followers_count, 
-            'following': tweet.author.following, 
-            'friends_count': tweet.author.friends_count, 
-            'geo_enabled': tweet.author.geo_enabled, 
-            'twitter_user_id': tweet.author.id, 
-            'lang': tweet.author.lang, 
-            'listed_count': tweet.author.listed_count, 
-            'location': tweet.author.location, 
-            'name': tweet.author.name, 
-            'notifications': tweet.author.notifications, 
-            'profile_image_url': tweet.author.profile_image_url, 
-            'protected': tweet.author.protected, 
-            'statuses_count': tweet.author.statuses_count, 
-            'time_zone': tweet.author.time_zone, 
-            'url': tweet.author.url, 
-            'utc_offset': tweet.author.utc_offset, 
-            'verified': tweet.author.verified,
-            '_updated': datetime.datetime.now(),
-            }
-            authors.append(u)
-            ts.append(t)        
-            print ts
-            self.TweetAMessage()
-            #self.api.send_direct_message("ozansener","selamlar")
+        print "Select a user:\n"
+        i=0
+        for fw in followers:
+            i+=1
+            print "%d - %s" % (i,fw.screen_name)
+            users[i]=fw.screen_name
+        choice = int(raw_input("make your choice?\n"))
         
-        if self.debug:
-            print "added %s tweets to the db" % (len(ts))
+        #try:
+        deger = str(users[choice])
+        
+        thread.start_new_thread(self.sendMessages,(deger,))
+        thread.start_new_thread(self.getMessage,())
+        #except:
+          #  print "Error: unable to start thread"
+        
+        
+##        for tweet in tweets:
+##           if tweet.author.screen_name=="ozansener" :
+##            print str(tweet.in_reply_to_status_id) + "\n"
+##            t = {
+##            'author': tweet.author.screen_name,
+##            # 'destroy': tweet.destroy,
+##            # 'favorite': tweet.favorite,
+##            'id': tweet.id,
+##            'in_reply_to_screen_name': tweet.in_reply_to_screen_name,
+##            'in_reply_to_status_id': tweet.in_reply_to_status_id,
+##            'in_reply_to_user_id': tweet.in_reply_to_user_id,
+##            # 'parse': tweet.parse,
+##            # 'parse_list': tweet.parse_list,
+##            # 'retweet': dir(tweet.retweet),
+##            # 'retweets': dir(tweet.retweets),
+##            # 'source_url': tweet.source_url,
+##            'text': tweet.text,
+##            'user': tweet.user.screen_name,
+##            }
+##            u = {
+##            '_id': tweet.author.screen_name, # use as mongo primary key
+##            'contributors_enabled': tweet.author.contributors_enabled, 
+##            'created_at': tweet.author.created_at, 
+##            'description': tweet.author.description, 
+##            'favourites_count': tweet.author.favourites_count, # beware the british
+##            'follow_request_sent': tweet.author.follow_request_sent, 
+##            'followers_count': tweet.author.followers_count, 
+##            'following': tweet.author.following, 
+##            'friends_count': tweet.author.friends_count, 
+##            'geo_enabled': tweet.author.geo_enabled, 
+##            'twitter_user_id': tweet.author.id, 
+##            'lang': tweet.author.lang, 
+##            'listed_count': tweet.author.listed_count, 
+##            'location': tweet.author.location, 
+##            'name': tweet.author.name, 
+##            'notifications': tweet.author.notifications, 
+##            'profile_image_url': tweet.author.profile_image_url, 
+##            'protected': tweet.author.protected, 
+##            'statuses_count': tweet.author.statuses_count, 
+##            'time_zone': tweet.author.time_zone, 
+##            'url': tweet.author.url, 
+##            'utc_offset': tweet.author.utc_offset, 
+##            'verified': tweet.author.verified,
+##            '_updated': datetime.datetime.now(),
+##            }
+##            authors.append(u)
+##            ts.append(t)        
+##        print ts
+##        self.TweetAMessage()
+            #self.api.send_direct_message("ozansener","selamlar")
     def TweetAMessage(self):
-        #self.api.send_direct_message(screen_name = "ozansener", text = "selamlar")
-        self.api.update_status(status="hackABit test",in_reply_to_status_id=54659309837090816L)
-        #self.api.retweet(54659309837090816L)
+        self.api.send_direct_message(screen_name = "ozansener", text = "selamlar")
+        #self.api.update_status(status="hackABit test",in_reply_to_status_id=54622552307412992)
+        #self.api.retweet(54622552307412992L)
     def __init__(self, debug=False):
         self.debug = debug
         auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
